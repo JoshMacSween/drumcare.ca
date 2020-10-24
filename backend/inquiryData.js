ed3f92fcbbeb2c0bd8f2398cb2c3d2f79be95de8
Joshua MacSween <joshmacsween@gmail.com>
2020-10-24 13:25:02 -0600

    inquiry routes

---
+++ backend/inquiryData.js
@@ -0,0 +1,31 @@
+const inquiry = [
+  {
+    _id: 1,
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    phoneNumber: '1234567',
+    kitSize: '5',
+    package: 'bronze',
+    inquiryText: 'I would like to book for the 16th please'
+  },
+  {
+    _id: 2,
+    name: 'Miles Workaby',
+    email: 'Miles@example.com',
+    phoneNumber: '7654321',
+    kitSize: '3',
+    package: 'gold',
+    inquiryText: 'Who knew drums had to be tuned?'
+  },
+  {
+    _id: 3,
+    name: 'Dane Jones',
+    email: 'Dane@example.com',
+    phoneNumber: '7803339999',
+    kitSize: '4',
+    package: 'platinum',
+    inquiryText: 'Does this come with fresh moon gel?'
+  },
+]
+
+module.exports = inquiry
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const inquiries = require('./inquiryData')
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running ')
+})
+
+app.get('/inquiry', (req, res) => {
+  res.send(inquiries)
+})
+
+app.get('/inquiry/:id', (req, res) => {
+  const inquiry = inquiries.find((i) => i._id === Number(req.params.id))
+  res.json(inquiry)
 })
 
 const PORT = process.env.PORT || 5000
